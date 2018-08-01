d38895ff7dd144ac42adc0df0983ca990886b20c
Mircea Popescu <mirceap@gmail.com>
2018-08-01 12:03:59 -0600

    Before modifying pictures

---
+++ ScrollViewDemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewDemo2
+//
+//  Created by Mircea Popescu on 8/1/18.
+//  Copyright © 2018 Mircea Popescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScrollViewDemo2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollViewDemo2
+//
+//  Created by Mircea Popescu on 8/1/18.
+//  Copyright © 2018 Mircea Popescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
